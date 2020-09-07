176bdbf668858756959115615e709d7d4b69fb7f
Stephen Moye <stephenmoye@gmail.com>
2020-09-06 23:37:36 -0700

    adjust linting style rules

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
-  "extends": "stylelint-config-standard",
-  "plugins": ["stylelint-scss"],
-  "rules": {
+  extends: "stylelint-config-standard",
+  plugins: ["stylelint-scss"],
+  rules: {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
-    "no-descending-specificity": null
+    "no-descending-specificity": null,
+    "declaration-empty-line-before": "never",
+    "rule-empty-line-before": "always",
   },
-}
+};
